ac1b110c3ef98d1e7509f1474c9c9ec1527bcfc3
matgor96 <matgor96@gmail.com>
2020-10-12 18:10:43 +0200

    Added functionality to the game

--- index.js
+++ index.js
@@ -1,56 +1,109 @@
 function startGame(...images) {
-    const imgTypesNum = images.length;
-    const gameContainer = document.querySelector('.container');
-    const width = parseInt(window.getComputedStyle(gameContainer).width);
-    let gameContent = new Array(imgTypesNum * 2);
-    gameContent.fill(null);
-    const result = [];
+  const imgTypesNum = images.length;
+  const gameContainer = document.querySelector('.container');
+  const width = parseInt(window.getComputedStyle(gameContainer).width);
+  let gameContent = new Array(imgTypesNum * 2);
+  gameContent.fill(null);
+  const result = [];
+  let storeElement;
+  let score = 0;
 
-    const handleLayout = [];
-    images.forEach(img => {
-        handleLayout.push({
-            name: img,
-            count: 0
-        });
+  const handleLayout = [];
+  images.forEach(img => {
+    handleLayout.push({
+      name: img,
+        count: 0
     });
+  });
 
-    gameContent.map(() => {        
-        checkIfAvailable(handleLayout,images, imgTypesNum, gameContent, result);
-    });
+  gameContent.map(() => {        
+    checkIfAvailable(handleLayout,images, imgTypesNum, gameContent, result);
+  });
 
-    const domContent = result.map(imgUrl => {
-        return `
-            <div 
-                style="
-                    background-image: url(${imgUrl});
-                    width: ${width / 4}px;
-                    heigth: ${width / 4}px;
-                " 
-                class="game-el"></div>
+  const domContent = result.map(imgUrl => {
+    return `
+      <div 
+        style="
+          background-image:
+            url(images/blank.png),
+            url(${imgUrl});
+            width: ${width / 4}px;
+            heigth: ${width / 4}px;
+        " 
+        class="game-el"></div>
         `
-    }).join('');
+  }).join('');
 
+  gameContainer.innerHTML = domContent;
     const gameEls = document.querySelectorAll('.game-el');
-    gameEls.forEach(element => {
-        element.addEventListener('click', () => {
-            
-        });
-    });
+    const h = [...gameEls]
 
-    gameContainer.innerHTML = domContent;
+      gameEls.forEach(element => {
+        element.addEventListener('click', function handleElements(e){
+          if(!storeElement) {
+            storeElement = e.target;
+            storeElement.style.backgroundImage = storeElement.style.backgroundImage.toString().split(',').reverse().join(',');
+          } else {
+            e.target.style.backgroundImage = e.target.style.backgroundImage.toString().split(',').reverse().join(',');
+                    
+            if(
+              window.getComputedStyle(storeElement).backgroundImage == window.getComputedStyle(e.target).backgroundImage &&
+              (storeElement.offsetLeft != e.target.offsetLeft ||
+              storeElement.offsetTop != e.target.offsetTop)
+            ) {
+              score++;
+              document.querySelector('h3').innerText = `Score: ${score}`;
+
+              setTimeout(() => {
+                e.target.style.backgroundImage = 'url(images/white.png)';
+                storeElement.style.backgroundImage = 'url(images/white.png)';
+                e.target.removeEventListener('click', handleElements);
+                storeElement.removeEventListener('click', handleElements);
+
+                if(!h.find(el => {
+                  return el.style.backgroundImage.includes('blank.png');
+                })) {
+                  gameContainer.innerHTML = `
+                    <h1>You win!</h1>
+                    <button>Start Again</button>
+                  `;
+                  document.querySelector('button').addEventListener('click', () => {
+                  gameContainer.innerHTML = '';
+                  document.querySelector('h3').innerText = `Score: 0`;
+                  startGame(
+                    'images/cheeseburger.png',
+                    'images/fries.png',
+                    'images/hotdog.png',
+                    'images/ice-cream.png',
+                    'images/milkshake.png',
+                    'images/pizza.png'
+                  );  
+                  });
+                }
+              }, 499);
+            } 
+            setTimeout(() => {
+
+              e.target.style.backgroundImage = e.target.style.backgroundImage.toString().split(',').reverse().join(',');
+              storeElement.style.backgroundImage = storeElement.style.backgroundImage.toString().split(',').reverse().join(',');
+              storeElement = null;
+            }, 500);
+          }
+        });
+      });
 }
 
 function checkIfAvailable(handleLayout, images, imgTypesNum, gameContent, result) {
-    const rand = Math.floor(Math.random() * imgTypesNum);
+  const rand = Math.floor(Math.random() * imgTypesNum);
 
-    let setGame = handleLayout.find(el => el.name == images[rand]);
-    if(setGame.count < 2) {
-        setGame.count++;
-        result.push(setGame.name);
-    } else {
-        checkIfAvailable(handleLayout, images, imgTypesNum, gameContent, result);
-    }
-}
+  let setGame = handleLayout.find(el => el.name == images[rand]);
+  if(setGame.count < 2) {
+    setGame.count++;
+    result.push(setGame.name);
+  } else {
+    checkIfAvailable(handleLayout, images, imgTypesNum, gameContent, result);
+  }
+};
 
 startGame(
     'images/cheeseburger.png',
